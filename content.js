63b164a719f987c8d02a175f18a30b13b9f18685
ananya4198 <115587326+ananya4198@users.noreply.github.com>
2024-04-21 16:55:45 -0400

    Delete content.js

--- content.js
+++
@@ -1,6 +0,0 @@
-// Listen for messages from the popup
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.action === "changeFont") {
-      document.body.style.fontFamily = message.fontFamily;
-    }
-  });
